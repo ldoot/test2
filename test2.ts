c306805b9e6367de9ac84e8ac02cfce73bfdefe2
LUCAS HOLMES <lucasholmes@hotmail.co.uk>
2024-10-11 09:42:21 +0100

    add linting

--- test2.ts
+++ test2.ts
@@ -3,29 +3,31 @@ import { createSqlQueryChain } from "langchain/chains/sql_db";
 import { SqlDatabase } from "langchain/sql_db";
 import { DataSource } from "typeorm";
 
-const datasource = new DataSource({
-  type: "sqlite",
-  database: "../../../../Chinook.db",
-});
-const db = await SqlDatabase.fromDataSourceParams({
-  appDataSource: datasource,
-});
+export const test2 = async () => {
+    const datasource = new DataSource({
+        type: "sqlite",
+        database: "../../../../Chinook.db",
+    });
+    const db = await SqlDatabase.fromDataSourceParams({
+        appDataSource: datasource,
+    });
 
-const llm = new ChatOpenAI({ model: "gpt-4", temperature: 0 });
-const chain = await createSqlQueryChain({
-  llm,
-  db,
-  dialect: "sqlite",
-});
+    const llm = new ChatOpenAI({ model: "gpt-4", temperature: 0 });
+    const chain = await createSqlQueryChain({
+        llm,
+        db,
+        dialect: "sqlite",
+    });
 
-const response = await chain.invoke({
-  question: "How many employees are there?",
-});
-console.log("response", response);
-/**
-response SELECT COUNT(*) FROM "Employee"
- */
-console.log("db run result", await db.run(response));
-/**
-db run result [{"COUNT(*)":8}]
- */
+    const response = await chain.invoke({
+     question: "How many employees are there?",
+    });
+    console.log("response", response);
+    /**
+    response SELECT COUNT(*) FROM "Employee"
+    */
+    console.log("db run result", await db.run(response));
+    /**
+    db run result [{"COUNT(*)":8}]
+    */
+}
