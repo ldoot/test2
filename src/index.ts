d120c81e2f99587313ded4ea2c819419d10e5c8a
LUCAS HOLMES <lucasholmes@hotmail.co.uk>
2024-10-11 12:09:03 +0100

    large db test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import { test3 } from './test3';
-// import {test1} from './test1'
-// import { test2 } from './test2';
-//test1();
-// test2();
-test3();
+
+import { highCardinalityTest } from './large_database/test2';
+
+highCardinalityTest()
